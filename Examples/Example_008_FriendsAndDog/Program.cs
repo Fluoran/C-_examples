a28c693aacfd1ac4eb1002b3e7b70b56b686cc18
Maksim <max.borisov@mail.ru>
2023-12-06 12:05:07 +0700

    Решение задачи с собакой и друзьями

---
+++ Examples/Example_008_FriendsAndDog/Program.cs
@@ -0,0 +1,27 @@
+﻿
+int count = 0,
+    distance = 10000,
+    firstFriendSpeed = 1,
+    secondFriendSpeed = 2,
+    dogSpeed = 5,
+    friend = 2,
+    time = 0;
+
+while (distance > 10)
+{
+    if (friend == 1)
+    {
+        time = distance / (secondFriendSpeed + dogSpeed);
+        friend = 2;
+    }
+    if (friend == 2)
+    {
+        time = distance / (firstFriendSpeed + dogSpeed);
+        friend = 1;
+    }
+    distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+    System.Console.WriteLine(distance);
+    count++;
+}
+System.Console.Write("Количество кругов которые пробежит собака = ");
+System.Console.WriteLine(count);
