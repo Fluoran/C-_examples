44c66d79c0d20273aba1aa8322e425370ab70f73
Maksim <max.borisov@mail.ru>
2023-12-06 10:52:34 +0700

    Особенное приветствие

--- Examples/Example_005/Program.cs
+++ Examples/Example_005/Program.cs
@@ -1,1 +1,12 @@
 ﻿
+System.Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+if (username.ToLower() == "максим")
+{
+    System.Console.WriteLine("Ура, это же Максим!");
+}
+else
+{
+    System.Console.WriteLine("Привет, ");
+    System.Console.WriteLine(username);
+}
